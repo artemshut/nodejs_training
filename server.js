8dd26c1d3add1f934dbe6521767b6614be62e6be
ashut-pc <artemshut@gmail.com>
2015-12-19 12:46:17 +0300

    [Lesson 6] work with db and logger modules

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-var user = require("./user");
+var log = require('logger')(module);
+var db = require('db');
+db.connect();
 
-var petya = new user.User("petya");
-var vasya = new user.User("vasya");
+var User = require("./user");
 
-vasya.hello(petya);
+function run() {
+    var petya = new User("petya");
+    var vasya = new User("vasya");
+
+    vasya.hello(petya);
+    log(db.getPhrase("Run successful"));
+}
+
+if (module.parent) {
+    exports.run = run;
+} else {
+    run();
+}
