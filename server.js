0125dde71361caebe080e56e7507d1d60346820a
ashut-pc <artemshut@gmail.com>
2015-12-25 16:40:36 +0300

    [ASYNC] async with fs and error convention

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var http  = require('http');
 var log = require('winston');
+var fs = require('fs');
 
-var server = http.createServer();
+var server = http.createServer(function (req, res) {
+    if (req.url == '/') {
+        fs.readFile('index.html', function (err, info) {
+            if (err) {
+                log.error(err);
+                res.statusCode = 500;
+                res.end("Internal server error");
+                return
+            }
+            res.end(info);
+        });
+    } else {
+        res.statusCode = 404;
+        res.end('Page not found')
+    }
+}).listen(3000);
 
-server.on('request', require('./request'));
-
-server.listen(1338);
-
-log.info("server is running");
