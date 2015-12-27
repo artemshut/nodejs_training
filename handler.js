1e4556cf380c8c13031183f6c72558ff121e82e0
ashut-pc <artemshut@gmail.com>
2015-12-27 20:39:22 +0300

    [Domains] async try catch

---
+++ handler.js
@@ -0,0 +1,14 @@
+//var fs = require('fs');
+
+var redis = require('redis').createClient();
+
+module.exports =  function handler(req, res) {
+    if (req.url == '/') {
+        redis.get("data", process.domain.bind(function(err, data) {
+           throw new Error('redis callback');
+        }));
+    } else {
+        res.statusCode = 404;
+        res.end("Not found");
+    }
+};
