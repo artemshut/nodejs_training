29335fae7cb77f75605560440d740d3f4dd9546d
ashut-pc <artemshut@gmail.com>
2016-01-03 20:14:49 +0300

    index.ejs

--- app.js
+++ app.js
@@ -6,65 +6,42 @@ var config = require('config');
 var log = require('libs/log')(module);
 
 var app = express();
-app.set('port', config.get('port'));
-http.createServer(app).listen(config.get('port'), function () {
-    log.info('Express port listening ' + config.get('port'));
-});
-
-// Middleware
-
-app.use(function (req, res, next) {
-   res.end("Hello");
-});
-
-app.use(function (req, res, next) {
-    if (req.url == '/test') {
-        next(new Error('wops, denied'));
-    } else {
-        next();
-    }
-});
-
-app.use(function (req, res) {
-    res.send(404, '404 error');
-});
-
-app.use(function (req, res, next, err) {
-    if (app.get('env') == 'development') {
-        var errorHandler = app.use(express.errorHandler());
-        errorHandler(err, req, res, next);
-    } else {
-        res.send(500);
-    }
-});
 
 //var app = module.exports = express.createServer();
 
 // Configuration
 
-//app.configure(function(){
-//  app.set('views', __dirname + '/views');
-//  app.set('view engine', 'jade');
-//  app.use(express.bodyParser());
-//  app.use(express.methodOverride());
-//  app.use(express.cookieParser());
-//  app.use(express.session({ secret: 'your secret here' }));
-//  app.use(app.router);
-//  app.use(express.static(__dirname + '/public'));
-//});
-//
-//app.configure('development', function(){
-//  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-//});
-//
-//app.configure('production', function(){
-//  app.use(express.errorHandler());
-//});
-//
-//// Routes
-//
-//app.get('/', routes.index);
-//
-//app.listen(3000, function(){
-//  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-//});
+if (app.get('env') == 'development') {
+    app.use(express.logger('dev'));
+} else {
+    app.use(express.logger('default'));
+}
+
+app.use('views', __dirname + '/templates');
+app.use('view engine', 'ejs');
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'your secret here' }));
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', function (req, res, next) {
+    res.render('index', {
+        body: "<b>Hello</b>>"
+    })
+});
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", config.get('port'), app.settings.env);
+});
